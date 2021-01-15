bb327a2c3ea50dc0bba1bacc7c4e5a40d3d6ee29
Sleepy Mouse <orange.lavender.9@gmail.com>
2021-01-15 19:44:42 +1100

    [DynamoDB] add Repayment table binding bean

--- src/main/java/cq/playground/home_loan/Repayment.java
+++ src/main/java/cq/playground/home_loan/Repayment.java
@@ -1,9 +1,13 @@
 package cq.playground.home_loan;
 
+import com.fasterxml.uuid.Generators;
+import cq.playground.home_loan.dynamodb.RepaymentItem;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
 
+@Slf4j
 @Data
 public class Repayment {
     final BigDecimal loanBalance;
@@ -21,5 +25,36 @@ public class Repayment {
         this.interestPaid = interestPaid;
         this.principalPaid = repaymentAmount.subtract(interestPaid);
         this.schedule = schedule;
+    }
+
+    public static Repayment build(
+            BigDecimal loanBalance,
+            BigDecimal repaymentAmount,
+            BigDecimal interestPaid,
+            RepaymentSchedule schedule
+    ) {
+        return new Repayment(
+                loanBalance,
+                repaymentAmount,
+                interestPaid,
+                schedule
+        );
+    }
+
+    public RepaymentItem dynamoDbItem() {
+        var uuid = Generators.randomBasedGenerator().generate();
+        log.info("UUID: {}", uuid);
+        log.info("UUID version: {}", uuid.version());
+        log.info("UUID variant: {}", uuid.variant());
+
+        var item = new RepaymentItem();
+        item.setRepaymentItemId(uuid.toString());
+        item.setLoanBalance(getLoanBalance());
+        item.setRepaymentAmount(getRepaymentAmount());
+        item.setInterestPaid(getInterestPaid());
+        item.setPrincipalPaid(getPrincipalPaid());
+        item.setSchedule(getSchedule());
+        log.info("{}", this);
+        return item;
     }
 }
---
+++ src/main/java/cq/playground/home_loan/dynamodb/RepaymentItem.java
@@ -0,0 +1,131 @@
+package cq.playground.home_loan.dynamodb;
+
+import cq.playground.home_loan.RepaymentSchedule;
+import lombok.Data;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.StaticTableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static java.math.MathContext.DECIMAL128;
+import static software.amazon.awssdk.enhanced.dynamodb.mapper.StaticAttributeTags.primaryPartitionKey;
+
+@Data
+@DynamoDbBean
+public class RepaymentItem {
+    public static final String TABLE_REPAYMENT = "Repayment";
+    public static final String ATTRIBUTE_REPAYMENT_ITEM_ID = "repaymentItemId";
+    public static final String ATTRIBUTE_LOAN_BALANCE = "loanBalance";
+    public static final String ATTRIBUTE_REPAYMENT_AMOUNT = "repaymentAmount";
+    public static final String ATTRIBUTE_INTEREST_PAID = "interestPaid";
+    public static final String ATTRIBUTE_PRINCIPAL_PAID = "principalPaid";
+    public static final String ATTRIBUTE_SCHEDULE = "schedule";
+    public static final String ATTRIBUTE_PAID_AT = "paidAt";
+
+    public static final TableSchema<RepaymentItem> TABLE_SCHEMA_REPAYMENT =
+            StaticTableSchema.builder(RepaymentItem.class).newItemSupplier(RepaymentItem::new)
+                    .addAttribute(String.class, a ->
+                            a.name(ATTRIBUTE_REPAYMENT_ITEM_ID)
+                                    .getter(RepaymentItem::getRepaymentItemId)
+                                    .setter(RepaymentItem::setRepaymentItemId)
+                                    .tags(primaryPartitionKey())
+                    )
+                    .addAttribute(BigDecimal.class, a ->
+                            a.name(ATTRIBUTE_LOAN_BALANCE)
+                                    .getter(RepaymentItem::getLoanBalance)
+                                    .setter(RepaymentItem::setLoanBalance)
+                    )
+                    .addAttribute(BigDecimal.class, a ->
+                            a.name(ATTRIBUTE_REPAYMENT_AMOUNT)
+                                    .getter(RepaymentItem::getRepaymentAmount)
+                                    .setter(RepaymentItem::setRepaymentAmount)
+                    )
+                    .addAttribute(BigDecimal.class, a ->
+                            a.name(ATTRIBUTE_INTEREST_PAID)
+                                    .getter(RepaymentItem::getInterestPaid)
+                                    .setter(RepaymentItem::setInterestPaid)
+                    )
+                    .addAttribute(BigDecimal.class, a ->
+                            a.name(ATTRIBUTE_PRINCIPAL_PAID)
+                                    .getter(RepaymentItem::getPrincipalPaid)
+                                    .setter(RepaymentItem::setPrincipalPaid)
+                    )
+                    .addAttribute(RepaymentSchedule.class, a ->
+                            a.name(ATTRIBUTE_SCHEDULE)
+                                    .getter(RepaymentItem::getSchedule)
+                                    .setter(RepaymentItem::setSchedule)
+                    )
+                    .addAttribute(LocalDateTime.class, a ->
+                            a.name(ATTRIBUTE_PAID_AT)
+                                    .getter(RepaymentItem::getPaidAt)
+                                    .setter(RepaymentItem::setPaidAt)
+                    )
+                    .build();
+
+    private String repaymentItemId;
+    private BigDecimal loanBalance;
+    private BigDecimal repaymentAmount;
+    private BigDecimal interestPaid;
+    private BigDecimal principalPaid;
+    private RepaymentSchedule schedule;
+    private LocalDateTime paidAt;
+
+    private static BigDecimal round(BigDecimal number) {
+        return number.round(DECIMAL128);
+    }
+
+    @DynamoDbPartitionKey
+    public String getRepaymentItemId() {
+        return repaymentItemId;
+    }
+
+    @DynamoDbAttribute(ATTRIBUTE_LOAN_BALANCE)
+    public BigDecimal getLoanBalance() {
+        return loanBalance;
+    }
+
+    public void setLoanBalance(BigDecimal loanBalance) {
+        this.loanBalance = round(loanBalance);
+    }
+
+    @DynamoDbAttribute(ATTRIBUTE_REPAYMENT_AMOUNT)
+    public BigDecimal getRepaymentAmount() {
+        return repaymentAmount;
+    }
+
+    public void setRepaymentAmount(BigDecimal repaymentAmount) {
+        this.repaymentAmount = round(repaymentAmount);
+    }
+
+    @DynamoDbAttribute(ATTRIBUTE_INTEREST_PAID)
+    public BigDecimal getInterestPaid() {
+        return interestPaid;
+    }
+
+    public void setInterestPaid(BigDecimal interestPaid) {
+        this.interestPaid = round(interestPaid);
+    }
+
+    @DynamoDbAttribute(ATTRIBUTE_PRINCIPAL_PAID)
+    public BigDecimal getPrincipalPaid() {
+        return principalPaid;
+    }
+
+    public void setPrincipalPaid(BigDecimal principalPaid) {
+        this.principalPaid = round(principalPaid);
+    }
+
+    @DynamoDbAttribute(ATTRIBUTE_SCHEDULE)
+    public RepaymentSchedule getSchedule() {
+        return schedule;
+    }
+
+    @DynamoDbAttribute(ATTRIBUTE_PAID_AT)
+    public LocalDateTime getPaidAt() {
+        return paidAt;
+    }
+}

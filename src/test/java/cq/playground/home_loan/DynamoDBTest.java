f644d2830aa73742fb6b41b54277c340c2ca1b8d
Sleepy Mouse <orange.lavender.9@gmail.com>
2021-01-15 19:46:08 +1100

    [DynamoDB] add scan test case

--- src/test/java/cq/playground/home_loan/DynamoDBTest.java
+++ src/test/java/cq/playground/home_loan/DynamoDBTest.java
@@ -3,12 +3,22 @@ package cq.playground.home_loan;
 import cq.playground.home_loan.dynamodb.DynamoDBUtils;
 import cq.playground.home_loan.dynamodb.RepaymentItem;
 import cq.playground.home_loan.util.PropertiesReader;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
+@Slf4j
 public class DynamoDBTest {
     @Test
     public void createTable() {
         var dbUtils = new DynamoDBUtils(new PropertiesReader());
         dbUtils.createTable("Repayment", RepaymentItem.ATTRIBUTE_REPAYMENT_ITEM_ID);
+    }
+
+    @Test
+    public void scan() {
+        var dbUtils = new DynamoDBUtils(new PropertiesReader());
+        dbUtils.scan().map(Objects::toString).forEach(log::info);
     }
 }
